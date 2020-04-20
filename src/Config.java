bb89963d4a0a136c75efec091e4e42b5a5800296
Monique Ostermann <m.ostermann@obwyse.com>
2020-04-20 15:20:11 +0200

    config-interface: comments added

--- src/Config.java
+++ src/Config.java
@@ -1,22 +1,31 @@
 import java.awt.*;
 
+/*
+ * interface that holds all
+ * basic settings for the game
+ */
+
 public interface Config {
+    //settings for the Board
     int BOARD_WIDTH = 500;
     int BOARD_HEIGHT = 500;
     int BOARD_BORDER_LEFT = 10;
     int BOARD_BORDER_RIGHT = 10;
     Color BACKGROUND = Color.BLACK;
 
+    //settings for the players character
     int SPACESHIP_WIDTH = 15;
     int SPACESHIP_HEIGHT = 25;
     Color SPACESHIP_COLOR = Color.BLUE;
 
+    //settings for the enemies
     int ENEMY_WIDTH = 15;
     int ENEMY_HEIGHT = 25;
     int ENEMY_SPEED = 20;
     Color ENEMY_COLOR = Color.RED;
     int ENEMY_ENCOUNTERS = 15;
 
+    //settings for the bullet
     int BULLET_WIDTH = 3;
     int BULLET_HEIGHT = 15;
     int BULLET_SPEED = 20;
