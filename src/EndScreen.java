fc73f302b11c9ca7de5d10fe1245ab390a6d6acb
Monique Ostermann <m.ostermann@obwyse.com>
2020-04-29 16:45:07 +0200

    Added StartScreen logo, GameOver & YouWin Image

--- src/EndScreen.java
+++ src/EndScreen.java
@@ -9,6 +9,7 @@ public class EndScreen extends JPanel {
         this.setVisible(true);
         this.setBackground(Color.BLACK);
         endText = new JLabel();
+        endText.setFont(new Font("Arial", Font.PLAIN, 40));
         endText.setForeground(Color.YELLOW);
         endText.setHorizontalAlignment(JLabel.CENTER);
         endText.setVerticalAlignment(JLabel.CENTER);
@@ -18,10 +19,28 @@ public class EndScreen extends JPanel {
 
     public void setEndText(boolean win) {
         if(win) {
-            endText.setText("Du hast gewonnen");
+            try {
+                JLabel jl=new JLabel();
+                jl.setIcon(new javax.swing.ImageIcon("ressources/img/you_win.png"));
+                jl.setHorizontalAlignment(JLabel.CENTER);
+                jl.setVerticalAlignment(JLabel.CENTER);
+                add(jl, BorderLayout.CENTER);
+            }catch (Exception ex){
+                endText.setText("Du hast gewonnen");
+            }
+
         }
         else {
-            endText.setText("Du hast verloren");
+            try {
+                JLabel jl=new JLabel();
+                jl.setIcon(new javax.swing.ImageIcon("ressources/img/game_over.png"));
+                jl.setHorizontalAlignment(JLabel.CENTER);
+                jl.setVerticalAlignment(JLabel.CENTER);
+                add(jl, BorderLayout.CENTER);
+            }catch (Exception ex){
+                endText.setText("Du hast verloren");
+            }
+
         }
     }
 }
--- src/StartScreen.java
+++ src/StartScreen.java
@@ -1,21 +1,37 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class StartScreen extends JPanel {
     private Button startButton;
     private boolean started =false;
 
     public StartScreen() {
+
         super(new BorderLayout(5, 5));
         this.setBackground(Color.BLACK);
         JPanel content = new JPanel(new GridLayout(3, 1));
         content.setBackground(Color.BLACK);
+        JLabel jl=new JLabel();
+        try {
+            jl.setIcon(new javax.swing.ImageIcon("ressources/img/logo.jpg"));
+            jl.setHorizontalAlignment(JLabel.CENTER);
+        }catch (Exception ex){
+            System.out.println(ex);
+        }
+
         startButton = new Button("Start");
+        startButton.setFont(new Font("Arial", Font.PLAIN, 40));
         startButton.setForeground(Color.YELLOW);
         startButton.setBackground(Color.BLACK);
         startButton.setVisible(true);
         startButton.addActionListener(e -> started = true);
-        content.add(new JLabel());
+
+        //content.add(new JLabel());
+        content.add(jl);
         content.add(startButton);
         content.add(new JLabel());
 
