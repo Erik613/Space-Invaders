1f1850293ff471e8907d0c328010ce6a126f1c46
Patrick <patrickmatern1997@gmail.com>
2020-04-30 09:33:35 +0200

    add comments to DrawableObject.java

--- src/DrawableObject.java
+++ src/DrawableObject.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * class represents all drawable Objects
+ */
 public abstract class DrawableObject {
     private int x;
     private int y;
@@ -17,6 +20,11 @@ public abstract class DrawableObject {
 
     }
 
+    /**
+     * set X Position for a drawable Object
+     * @param x X position on screen
+     * @throws Exception if X Position is out of the Screen
+     */
     public void setX(int x) throws Exception{
         if(x > 0 && x < Screen.dimensionX - 50) {
             this.x = x;
@@ -27,10 +35,19 @@ public abstract class DrawableObject {
 
     }
 
+    /**
+     * getter for X Position
+     * @return X Position
+     */
     public int getX() {
         return this.x;
     }
 
+    /**
+     * set Y Position for a drawable Object
+     * @param y Y position on screen
+     * @throws Exception if Y Position is out of the Screen
+     */
     public void setY(int y) throws Exception{
         if(y > 0 && y < Screen.dimensionY - 50) {
             this.y = y;
@@ -38,33 +55,64 @@ public abstract class DrawableObject {
             throw new Exception("Object außerhalb des Bildes");
     }
 
+    /**
+     * getter for Y Position
+     * @return Y Position
+     */
     public int getY() {
         return this.y;
     }
 
+    /**
+     * getter for height of a Object
+     * @return height of a Object
+     */
     public int getHeight() {
         return height;
     }
 
+    /**
+     * getter for width of a Object
+     * @return width of a Object
+     */
     public int getWidth() {
         return width;
     }
+
+    /**
+     * set height of a Object
+     * @param height height of the Object
+     */
     public void setHeight(int height) {
         this.height = height;
     }
 
+    /**
+     * set width of a Object
+     * @param width width of the Object
+     */
     public void setWidth(int width) {
         this.width = width;
     }
 
+    /**
+     * @return
+     */
     public Image getImg() {
         return img;
     }
 
+    /**
+     * @param r
+     * @return
+     */
     public boolean hit (DrawableObject r) {
         return x < r.getX() + r.getWidth() && x + width > r.getX() && y < r.getY() + r.getHeight() && y + height > r.getY();
     }
 
+    /**
+     *
+     */
     public abstract void destroy();
 }
 
